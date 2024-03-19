f0b4beb25ef1e7a9733d1b85cf6154ff84d8dc7c
dudong2 <leesj9476lsj@gmail.com>
2024-03-19 19:10:39 +0900

    sub module call Store()

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dudong2/tttt
 
 go 1.21.4
+
+require github.com/dudong2/tttt/store v0.0.1 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/dudong2/tttt/store"
 )
 
 func main() {
+	store.Store()
 	fmt.Println("hello")
 }
